9e95db67c2ba90691bff0bf70d41e5e10a76c1fc
Dan Brubaker <danbru@gmail.com>
2024-03-09 12:19:10 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Brubaker Design Services Block Starter Theme
+
+This is the custom starter theme for Brubaker Design Services. It is a block-based theme perfect for quick Wordpress developement.
+
+## Change Log
+
+1.0.0 – Initial Launch.
---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Brubaker Design Services' Starter Theme
+ *
+ * @package BrubakerDesignServices\BDSStarterTheme
+ * @since   1.0.0
+ * @author  Dan Brubaker
+ * @link    https://brubakerservices.org/
+ * @license GPL-2.0+
+ */
